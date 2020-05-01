49f1da3a900451e53cb26606e2cab6b18b395217
Mateus Pandino <matpandino@gmail.com>
2020-05-01 00:33:16 -0300

    Add Multer + Auth + Upload

--- n1/frontend/src/App.js
+++ n1/frontend/src/App.js
@@ -1,48 +1,44 @@
-import React, { Fragment, useState, useEffect } from 'react'
-import Header from './Header'
-import './App.css'
-import backGrouinImg from './assets/dog.jpg'
-import api from './services/api'
+import React, { Fragment, useState, useEffect } from "react";
+import Header from "./Header";
+import "./App.css";
+import backGrouinImg from "./assets/dog.jpg";
+import api from "./services/api";
 
- 
-
-
-function App () {
-  const [projects, setProjects] = useState([]);8
+function App() {
+  const [projects, setProjects] = useState([]);
 
   useEffect(() => {
-      api.get('projects').then(res => {
-        setProjects(res.data);
-      })
-
-  }, [])
+    api.get("projects").then((res) => {
+      setProjects(res.data);
+    });
+  }, []);
 
   async function handleAddProject() {
     //  setProjects([...projects, `Novo Projeto  ${Date.now()}`])
 
-     const res = await api.post('projects',{
-      "title": `Novo Projeto  ${Date.now()}`,
-      "owner": "Jose Luiz"
+    const res = await api.post("projects", {
+      title: `Novo Projeto  ${Date.now()}`,
+      owner: "Jose Luiz",
     });
- 
-    setProjects([...projects, res.data] ); 
+
+    setProjects([...projects, res.data]);
   }
 
   return (
-     <>
-        <Header title="Homepage"/>
+    <>
+      <Header title="Homepage" />
 
-        <img width="300" src={backGrouinImg} alt=""/>
-        <ul>
-             {projects.map(a => (
-              <li key={a.id}>{a.title}</li>
-            ))} 
-         </ul>
-         <button type="button" onClick={handleAddProject}>Adicionar</button>
-     </>
-    )
-
+      <img width="300" src={backGrouinImg} alt="" />
+      <ul>
+        {projects.map((a) => (
+          <li key={a.id}>{a.title}</li>
+        ))}
+      </ul>
+      <button type="button" onClick={handleAddProject}>
+        Adicionar
+      </button>
+    </>
+  );
 }
-
 
 export default App;
